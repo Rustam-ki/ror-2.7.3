45a8859f6088e1408bc93b8aa3abfc522dc84acc
Rustam <bashbez.ru@yandex.ru>
2021-11-22 23:39:46 +0500

    new migrate

--- db/migrate/20211120114001_create_tests.rb
+++ db/migrate/20211120114001_create_tests.rb
@@ -3,11 +3,11 @@ class CreateTests < ActiveRecord::Migration[6.1]
     create_table :tests do |t|
       t.string :title, null: false
       t.integer :level, null: false, from: nil, to: 1
-      t.integer :category_id, null: false
 
-      t.references :category, null: false, foreign: true
-      t.references :user,  null: false, foreign: true
+      t.references :category, null: false, foreign_key: true
+      t.references :author,  null: false, foreign_key: { to_table: :users }
       t.timestamps
     end
+
   end
 end
--- db/migrate/20211120122849_create_answers.rb
+++ db/migrate/20211120122849_create_answers.rb
@@ -2,9 +2,9 @@ class CreateAnswers < ActiveRecord::Migration[6.1]
   def change
     create_table :answers do |t|
       t.text :body, null: false
-      t.boolean :correct, from: nil, to: false
+      t.boolean :correct, null: false, from: nil, to: false
 
-      t.references :test, null: false, foreign: true
+      t.references :test, null: false, foreign_key: true
       t.timestamps
     end
   end
