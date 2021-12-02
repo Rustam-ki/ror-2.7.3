e161063a404bfb0c75419b5913d17a3529a5be9d
Rustam <bashbez.ru@yandex.ru>
2021-12-03 00:02:58 +0500

    edit4

--- db/schema.rb
+++ db/schema.rb
@@ -39,11 +39,13 @@ ActiveRecord::Schema.define(version: 2021_11_28_160116) do
     t.string "title", null: false
     t.integer "level", default: 1, null: false
     t.integer "category_id", null: false
+    t.integer "user_id", null: false
     t.integer "author_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
   create_table "user_performed_tests", force: :cascade do |t|
@@ -64,6 +66,7 @@ ActiveRecord::Schema.define(version: 2021_11_28_160116) do
   add_foreign_key "answers", "tests"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users"
   add_foreign_key "tests", "users", column: "author_id"
   add_foreign_key "user_performed_tests", "tests"
   add_foreign_key "user_performed_tests", "users"
