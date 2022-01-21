428b2f8660ca62b74c6ac79bdb48e6941852076b
Rustam <bashbez.ru@yandex.ru>
2022-01-21 17:20:51 +0500

    lesson17

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,12 +62,13 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "TestGuru_production"
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'hidden-dawn-71925.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
-    port: 587,
+    domain: 'gmail.com',
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
     authentication: 'plain',
