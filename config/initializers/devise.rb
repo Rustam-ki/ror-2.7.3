909408d962c62100d58580010c2b8daee802acb0
Rustam <bashbez.ru@yandex.ru>
2022-01-21 18:49:41 +0500

    lesson17

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'abuab2021@gmail.com'
+  config.mailer_sender = 'qwertrondell@gmail.com'
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'Devise::Mailer'
