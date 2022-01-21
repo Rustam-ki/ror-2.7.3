9136646f5c8150edb751a1b7720c8b577cb109cf
Rustam <bashbez.ru@yandex.ru>
2022-01-21 18:26:49 +0500

    lesson17

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'gmail.com',
     user_name: 'abuab2021@gmail.com',
     password: 'stroika2020',
     authentication: 'plain',
