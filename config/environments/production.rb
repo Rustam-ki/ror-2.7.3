9bda0df570a807b6b3db1bc80951960c504190ac
Rustam <bashbez.ru@yandex.ru>
2022-01-21 18:12:06 +0500

    lesson17

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    user_name: abuab2021,
-    password: stroika2020,
+    user_name: ENV['SMTP_USERNAME'],
+    password: ENV['SMTP_PASSWORD'],
     authentication: 'plain',
     enable_starttls_auto: true  }
 
