2138648a14e37947da09353d488ee5cba1adad80
Rustam <bashbez.ru@yandex.ru>
2022-01-27 17:24:56 +0500

    lesson17

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     domain: 'gmail.com',
     port: 587,
-    user_name: 'rustam-ki',
+    user_name: 'qwertrondell',
     password: '89656696438rondell',
     authentication: 'plain',
     enable_starttls_auto: true  }
