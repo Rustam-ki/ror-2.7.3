900d8311221629ad9c64fafbab0db291c5b5c16b
Rustam <bashbez.ru@yandex.ru>
2022-03-10 12:56:25 +0500

    3

--- app/javascript/utilites/timer.js
+++ app/javascript/utilites/timer.js
@@ -1,28 +1,34 @@
-document.addEventListener('turbolinks:load', function() {
-    var form = document.getElementById('navform')
-    var remaining_time = form.dataset.differenceTime
-    var min = Math.floor(remaining_time / 60)
-    var sec = remaining_time % 60
+document.addEventListener('turbolinks:load', function () {
+    var form = document.getElementsByClassName('timer')
 
-    document.getElementById('seconds').innerHTML = sec
-    document.getElementById('minutes').innerHTML = min
-
-    setTimeout(timer, 1000)
-    function timer() {
-        sec--
+    if (form.length) {
+        var remaining_time = form.dataset.differenceTime
+        var min = Math.floor(remaining_time / 60)
+        var sec = remaining_time % 60
 
         document.getElementById('seconds').innerHTML = sec
+        document.getElementById('minutes').innerHTML = min
 
-        if (min == 0 && sec == 0) {
-            document.forms['navform'].submit()
-        }
+        setTimeout(timer, 1000)
 
-        if (sec == 0) {
-            min--
-            document.getElementById('minutes').innerHTML = min
-            sec = 59
-        } else {
-            setInterval(timer, 1000)
+        function timer() {
+            sec--
+
+            document.getElementById('seconds').innerHTML = sec
+
+            if (min == 0 && sec == 0) {
+                document.forms['navform'].submit()
+            }
+
+            if (sec == 0) {
+                min--
+                document.getElementById('minutes').innerHTML = min
+                sec = 59
+            } else {
+                setInterval(timer, 1000)
+            }
         }
+    } else {
+        return
     }
 })
